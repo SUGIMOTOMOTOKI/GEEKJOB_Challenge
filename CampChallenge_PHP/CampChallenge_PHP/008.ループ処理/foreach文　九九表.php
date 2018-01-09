4de9f8edc036b3daea6afb1b6df82ded3b64ad40
kuroneko\user <runowaaru96@gmail.com>
2018-01-09 22:59:12 +0900

    foreach文　九九表

---
+++ CampChallenge_PHP/CampChallenge_PHP/008.ループ処理/foreach文　九九表.php
@@ -0,0 +1,11 @@
+<?php
+
+$data1 = array(1,2,3,4,5,6,7,8,9);
+$data2 = array(1,2,3,4,5,6,7,8,9);
+   foreach ($data1 as $x) {
+   foreach ($data2 as $y) {
+        printf("%02d ", $x * $y);
+        }
+    echo "<br>";
+}
+?>
