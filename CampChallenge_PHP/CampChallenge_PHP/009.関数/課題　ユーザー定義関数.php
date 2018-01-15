6fa805fe8605da13b87061ac300f7a8ae63d4573
kuroneko\user <runowaaru96@gmail.com>
2018-01-15 22:51:22 +0900

    課題　ユーザー定義関数

---
+++ CampChallenge_PHP/CampChallenge_PHP/009.関数/課題　ユーザー定義関数.php
@@ -0,0 +1,12 @@
+<?php
+
+function myprofile(){
+         echo "私の名前は杉本元希です<br>";
+         echo "生年月日は昭和53年11月14日です<br>";
+         echo "趣味はゲームと生放送です<br>";
+         echo '<br>';
+}
+
+for($i=0;$i<10;$i++){
+  echo myprofile();
+}
