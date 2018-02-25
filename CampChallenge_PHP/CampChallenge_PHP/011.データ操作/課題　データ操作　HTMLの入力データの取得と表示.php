8ee5034ec1a04855102afcdfc701ee7a3e99262f
kuroneko\user <runowaaru96@gmail.com>
2018-02-25 19:56:58 +0900

    課題　データ操作　HTMLの入力データの取得と表示

---
+++ CampChallenge_PHP/CampChallenge_PHP/011.データ操作/課題　データ操作　HTMLの入力データの取得と表示.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+  <title>コントロールサンプル</title>
+</head>
+<body>
+  <form action="./sample.php" method="post">
+    名前<input type="text" name="txtName">
+    性別:
+    男<input type="radio" name="sex" value="man">
+    女<input type="radio" name="sex" value="woman">
+    趣味<textarea name="mulText"></textarea>
+  </form>
+</body>
+</html>
